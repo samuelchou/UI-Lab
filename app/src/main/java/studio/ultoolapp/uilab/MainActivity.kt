ba0857f82d38c4cf7133822a69aedd7acf3c0df3
Samuel Chou <samuelchou@hotmail.com.tw>
2021-07-27 18:33:10 +0800

    feat(main/view): 將 MainActivity 改寫為 Data Binding 結構。

--- app/src/main/java/studio/ultoolapp/uilab/MainActivity.kt
+++ app/src/main/java/studio/ultoolapp/uilab/MainActivity.kt
@@ -2,10 +2,17 @@ package studio.ultoolapp.uilab
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import studio.ultoolapp.uilab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.textView.text = "Hello Data Binding"
     }
 }
