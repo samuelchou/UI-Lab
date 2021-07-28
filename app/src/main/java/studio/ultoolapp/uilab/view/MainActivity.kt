78d081c34d85267d51be19c0e8725df59f5b788a
Samuel Chou <samuelchou@hotmail.com.tw>
2021-07-28 10:56:51 +0800

    feat(main/view): 實裝 ViewPager 與 BottomNavigationView. 現在可以正確顯示兩個分頁了！

--- app/src/main/java/studio/ultoolapp/uilab/view/MainActivity.kt
+++ app/src/main/java/studio/ultoolapp/uilab/view/MainActivity.kt
@@ -3,6 +3,9 @@ package studio.ultoolapp.uilab.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import studio.ultoolapp.uilab.R
 import studio.ultoolapp.uilab.databinding.ActivityMainBinding
 
@@ -14,6 +17,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        // TODO: 2021/7/27 setup fragments...
+        setupPages()
+    }
+
+    private fun setupPages() {
+        binding.viewPager.run {
+            adapter = object : FragmentStateAdapter(this@MainActivity) {
+                override fun getItemCount(): Int {
+                    return 2
+                }
+
+                override fun createFragment(position: Int): Fragment {
+                    return when (position) {
+                        0 -> ListFragment()
+                        else -> PullUpRefreshFragment()
+                    }
+                }
+            }
+
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bottomNavigationView.menu.getItem(position).isChecked = true
+                }
+            })
+
+            binding.bottomNavigationView.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.listFragment -> setCurrentItem(0, true)
+                    else -> setCurrentItem(1, true)
+                }
+                true
+            }
+        }
+
     }
 }
