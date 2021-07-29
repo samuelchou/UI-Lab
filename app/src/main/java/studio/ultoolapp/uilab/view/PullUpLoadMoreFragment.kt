15f07c39cdbecdb7a928e9e0088b705ab3699e99
Samuel Chou <samuelchou@hotmail.com.tw>
2021-07-29 17:26:05 +0800

    feat(pull-up/view): 抽換為 RecyclerView. 現在可以顯示預設 30 筆的清單資料。

--- app/src/main/java/studio/ultoolapp/uilab/view/PullUpLoadMoreFragment.kt
+++ app/src/main/java/studio/ultoolapp/uilab/view/PullUpLoadMoreFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import studio.ultoolapp.uilab.databinding.FragmentPullUpLoadMoreBinding
+import studio.ultoolapp.uilab.view.component.SimpleData
+import studio.ultoolapp.uilab.view.component.SimpleItemAdapter
 
 class PullUpLoadMoreFragment : Fragment() {
 
@@ -18,5 +21,28 @@ class PullUpLoadMoreFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentPullUpLoadMoreBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupList()
+    }
+
+    private fun setupList() {
+        val mAdapter = SimpleItemAdapter()
+        binding.itemContainer.run {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = mAdapter
+        }
+
+        mAdapter.submitList(getDummyList(30))
+    }
+
+    private fun getDummyList(size: Int): List<SimpleData> {
+        return mutableListOf<SimpleData>().apply {
+            for (i in 0 until size) {
+                add(SimpleData("ID%03d".format(i), "Text Title $i"))
+            }
+        }
     }
 }
