595e411e52992cdec59f117362c3b09c52307dbb
Samuel Chou <samuelchou@hotmail.com.tw>
2021-07-29 15:28:52 +0800

    feat(list/view): 引入 SwipeRefreshLayout. 現在可以進行下拉刷新了！

--- app/src/main/java/studio/ultoolapp/uilab/view/ListFragment.kt
+++ app/src/main/java/studio/ultoolapp/uilab/view/ListFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import studio.ultoolapp.uilab.databinding.FragmentListBinding
+import java.util.*
+import kotlin.concurrent.schedule
 
 class ListFragment : Fragment() {
 
@@ -18,5 +20,26 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupSwipeRefresh()
+    }
+
+    private fun setupSwipeRefresh() {
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            refreshData()
+        }
+    }
+
+    private fun refreshData() {
+        binding.swipeRefreshLayout.isRefreshing = true // reset loading status
+        Timer().schedule(800) {
+            binding.root.post {
+                binding.textView.text = "Hello List\nTime: ${System.currentTimeMillis()}"
+                binding.swipeRefreshLayout.isRefreshing = false // reset loading status
+            }
+        }
     }
 }
