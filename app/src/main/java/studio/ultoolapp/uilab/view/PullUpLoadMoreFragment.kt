c9c852251527908a56b2d535265ad35ef2aff94c
Samuel Chou <samuelchou@hotmail.com.tw>
2021-07-30 15:30:39 +0800

    feat(pull-up/view): 加裝簡單監聽器，當滑動到底端時會跳出提示訊息。

--- app/src/main/java/studio/ultoolapp/uilab/view/PullUpLoadMoreFragment.kt
+++ app/src/main/java/studio/ultoolapp/uilab/view/PullUpLoadMoreFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import studio.ultoolapp.uilab.databinding.FragmentPullUpLoadMoreBinding
 import studio.ultoolapp.uilab.view.component.SimpleData
 import studio.ultoolapp.uilab.view.component.SimpleItemAdapter
@@ -16,7 +18,7 @@ class PullUpLoadMoreFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentPullUpLoadMoreBinding.inflate(inflater, container, false)
@@ -33,6 +35,19 @@ class PullUpLoadMoreFragment : Fragment() {
         binding.itemContainer.run {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
             adapter = mAdapter
+
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    val lastVisibleItem =
+                        (layoutManager as LinearLayoutManager).findLastCompletelyVisibleItemPosition()
+                    val lastItem = mAdapter.itemCount - 1
+                    if (lastVisibleItem >= lastItem) {
+                        Toast.makeText(context, "Reached bottom.", Toast.LENGTH_SHORT).show()
+                        // TODO: 2021/7/30 load more data
+                    }
+                }
+            })
         }
 
         mAdapter.submitList(getDummyList(30))
