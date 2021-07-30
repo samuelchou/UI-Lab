ecfa97ca9df4be2421258a84704d2c617a212864
Samuel Chou <samuelchou@hotmail.com.tw>
2021-07-30 15:02:49 +0800

    feat(list/view): 抽換為 RecyclerView. 現在可以顯示預設 30 筆的清單資料。

--- app/src/main/java/studio/ultoolapp/uilab/view/ListFragment.kt
+++ app/src/main/java/studio/ultoolapp/uilab/view/ListFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import studio.ultoolapp.uilab.databinding.FragmentListBinding
+import studio.ultoolapp.uilab.view.component.SimpleData
+import studio.ultoolapp.uilab.view.component.SimpleItemAdapter
 import java.util.*
 import kotlin.concurrent.schedule
 
 class ListFragment : Fragment() {
 
     private lateinit var binding: FragmentListBinding
+    private var mAdapter: SimpleItemAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +28,18 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupList()
         setupSwipeRefresh()
+    }
+
+    private fun setupList() {
+        mAdapter = SimpleItemAdapter()
+        binding.itemContainer.run {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = mAdapter
+        }
+
+        mAdapter?.submitList(getDummyList(System.currentTimeMillis()))
     }
 
     private fun setupSwipeRefresh() {
@@ -37,8 +52,16 @@ class ListFragment : Fragment() {
         binding.swipeRefreshLayout.isRefreshing = true // reset loading status
         Timer().schedule(800) {
             binding.root.post {
-                binding.textView.text = "Hello List\nTime: ${System.currentTimeMillis()}"
+                mAdapter?.submitList(getDummyList(System.currentTimeMillis()))
                 binding.swipeRefreshLayout.isRefreshing = false // reset loading status
+            }
+        }
+    }
+
+    private fun getDummyList(time: Long): List<SimpleData> {
+        return mutableListOf<SimpleData>().apply {
+            for (i in 0 until 30) {
+                add(SimpleData("ID%03d".format(i), "Text Title $i (Refreshed at $time)"))
             }
         }
     }
