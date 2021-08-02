cac9ab17a92203340e2dbf5b487baf531882b29b
Samuel Chou <samuelchou@hotmail.com.tw>
2021-08-02 16:10:37 +0800

    refactor(component/view): 重命名 Type 參數，以便更好閱讀。

--- app/src/main/java/studio/ultoolapp/uilab/view/component/EasyBindingAdapter.kt
+++ app/src/main/java/studio/ultoolapp/uilab/view/component/EasyBindingAdapter.kt
@@ -10,26 +10,26 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class EasyBindingAdapter<T, VB : ViewDataBinding>(
-    @LayoutRes private val bindingLayoutId: Int,
-    diff: DiffUtil.ItemCallback<T>,
+abstract class EasyBindingAdapter<ITEM, ITEM_VB : ViewDataBinding>(
+    @LayoutRes private val itemLayoutId: Int,
+    diff: DiffUtil.ItemCallback<ITEM>,
     private val lifecycleOwner: LifecycleOwner? = null,
-) : ListAdapter<T, EasyBindingAdapter.AbViewHolder<VB>>(diff) {
+) : ListAdapter<ITEM, EasyBindingAdapter.AbViewHolder<ITEM_VB>>(diff) {
 
     class AbViewHolder<VB : ViewDataBinding>(val binding: VB) :
         RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AbViewHolder<VB> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AbViewHolder<ITEM_VB> {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val binding: VB =
-            DataBindingUtil.inflate(layoutInflater, bindingLayoutId, parent, false)
+        val binding: ITEM_VB =
+            DataBindingUtil.inflate(layoutInflater, itemLayoutId, parent, false)
         lifecycleOwner?.let { binding.lifecycleOwner = it }
         return AbViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: AbViewHolder<VB>, position: Int) {
+    override fun onBindViewHolder(holder: AbViewHolder<ITEM_VB>, position: Int) {
         bindData(holder.binding, position, getItem(position))
     }
 
-    abstract fun bindData(binding: VB, position: Int, data: T)
+    abstract fun bindData(binding: ITEM_VB, position: Int, data: ITEM)
 }
