de8b5a54a8ffff482dbd98a6a32357d14af372fd
Samuel Chou <samuelchou@hotmail.com.tw>
2022-05-24 14:51:48 +0800

    feat(entrance): 示範如何使用 ChipDrawable.

--- app/src/main/java/studio/ultoolapp/uilab/view/EntranceActivity.kt
+++ app/src/main/java/studio/ultoolapp/uilab/view/EntranceActivity.kt
@@ -1,7 +1,13 @@
 package studio.ultoolapp.uilab.view
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ImageSpan
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.chip.ChipDrawable
+import studio.ultoolapp.uilab.R
 import studio.ultoolapp.uilab.databinding.ActivityEntranceBinding
 
 class EntranceActivity : AppCompatActivity() {
@@ -22,5 +28,19 @@ class EntranceActivity : AppCompatActivity() {
         binding.btnView.setOnClickListener {
             CustomAdapterActivity.start(this)
         }
+
+        val chipDrawable = ChipDrawable.createFromResource(this, R.xml.intext_chip)
+        chipDrawable.apply {
+            setTextColor(Color.GREEN)
+            text = "CHIP"
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+        }
+        val span = ImageSpan(chipDrawable)
+
+        val spannableString = SpannableString(binding.textView.text)
+        spannableString.setSpan(
+            span, 6, spannableString.length, Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
+        binding.textView.text = spannableString
     }
 }
